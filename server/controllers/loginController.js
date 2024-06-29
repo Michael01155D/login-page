dfceedb4f35d53c7043d96bf575c347ba0231621
Michael Mroz <michaelmroz01111@gmail.com>
2024-06-29 14:56:07 -0300

    add jwt token authentication to login route. next todo: create front end login and landing pages

--- server/controllers/loginController.js
+++ server/controllers/loginController.js
@@ -2,10 +2,22 @@ const User = require("../models/User");
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcrypt');
+const token = require('jsonwebtoken');
 
+//send username and pw as request data
 router.post("/", async (req, res) => {
-    const user = await User.findById(req.body.id);
-    const isValid = await bcrypt.compare(req.body.password, user.passwordHash);
+    const user = await User.findOne({username: req.body.username});
+    const isValid = !user ? 
+    false 
+    : 
+    await bcrypt.compare(req.body.password, user.passwordHash);
+    if (!isValid) {
+        return res.status(401).json({
+            error: "Invalid Username or Password"
+        })
+    }
+    const authToken = token.sign(user.toJSON(), process.env.SECRET);
+    res.status(200).send({authToken, user});
 
 })
 
